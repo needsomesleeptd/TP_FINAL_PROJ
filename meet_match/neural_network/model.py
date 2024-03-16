f88697258f6bf9467bd96748e5c38536cdafdc90
Andrew Razin <razin.andrew.ru@mail.ru>
2024-03-16 18:03:29 +0300

    hardcoded everything

--- meet_match/neural_network/model.py
+++ meet_match/neural_network/model.py
@@ -4,6 +4,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 import pandas as pd
 import pickle
 from data_prepare import preprocess_all_texts, preprocess_text
+import json
 
 TOKEN_PATTERN = "[a-zA-Zа-яА-ЯёЁ]+"
 
@@ -18,21 +19,28 @@ def train(csv_file, vectorizer_file, vectorized_matrix_file):
     with open(vectorized_matrix_file, 'wb') as file:
         pickle.dump(vectorized_matrix, file)
 
-def predict(vectorizer_file, vectorized_matrix_file, sample_quary):
+def load(vectorizer_file, vectorized_matrix_file):
     with open(vectorizer_file, 'rb') as file:
         vectorizer = pickle.load(file)
-    with open( vectorized_matrix_file, 'rb') as file:
+    with open(vectorized_matrix_file, 'rb') as file:
         vectorized_matrix = pickle.load(file)
+    return vectorizer, vectorized_matrix
+
+def predict(csv_file, vectorizer, vectorized_matrix, sample_quary):
     sample_query = preprocess_text(sample_quary, TOKEN_PATTERN)
     qery_tdidf = vectorizer.transform([sample_query])
     cosine_similarities = cosine_similarity(qery_tdidf, vectorized_matrix)
     top_indices = cosine_similarities.argsort()[0][-5:][::-1]
     res = []
+    df = pd.read_csv(csv_file)
     for idx in top_indices:
         res.append({
-            "idx": idx,
+            "idx": int(idx),
+            "title": df["title"][idx],
+            "image": json.loads(df["images"][idx].replace("'", '"'))[0]["image"],
             "cosine_similarities": cosine_similarities[0][idx]
         })
     return res
+
 
 # train('dist.csv', 'vectorizer.pkl', 'vectorized_matrix.pkl')
