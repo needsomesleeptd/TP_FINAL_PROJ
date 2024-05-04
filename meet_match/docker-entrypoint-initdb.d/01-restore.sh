eecd230833593166ace5543b65e4dc93743f7c32
Buldakov Mikhail <mishabul2003@gmail.com>
2024-05-04 22:53:01 +0300

    now bd builds

--- meet_match/docker-entrypoint-initdb.d/01-restore.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-file="/docker-entrypoint-initdb.d/dump.pgdata"
-dbname=meetmatch_db
-
-echo "Restoring DB using $file"
-pg_restore -U postgres --dbname=$dbname --verbose --single-transaction < "$file" || exit 1
