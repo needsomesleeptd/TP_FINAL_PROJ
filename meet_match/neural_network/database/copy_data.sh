12a48dbe1ac9b01a67cf734b0de11ce655e35094
Buldakov Mikhail <mishabul2003@gmail.com>
2024-05-07 22:36:31 +0300

    directories changed

--- meet_match/neural_network/database/copy_data.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env
-bd_name="pgadmin4_container"
-docker cp ./dist_for_pg.csv $bd_name:/
-docker cp ./embeddings.csv $bd_name:/
--- meet_match/neural_network/database/copy_data.sql
+++
@@ -1,2 +0,0 @@
-\copy sponsor FROM './data/sponsor.csv' DELIMITER ',' CSV
-\copy team FROM './data/team.csv' DELIMITER ',' CSV
