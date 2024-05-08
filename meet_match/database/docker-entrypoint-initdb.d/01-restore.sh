a074ae20c028690872d5641ac82f626bf5e9f16f
Buldakov Mikhail <mishabul2003@gmail.com>
2024-05-09 00:17:51 +0300

    merged together with front

--- meet_match/database/docker-entrypoint-initdb.d/01-restore.sh
+++ meet_match/database/docker-entrypoint-initdb.d/01-restore.sh
@@ -1,6 +1,4 @@
-
 file="/docker-entrypoint-initdb.d/dump.pgdata"
 dbname=meetmatch_db
-
 echo "Restoring DB using $file"
 pg_restore -U any1 --dbname=$dbname --verbose --single-transaction < "$file" || exit 1
