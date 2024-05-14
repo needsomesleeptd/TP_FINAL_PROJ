1f3c5d0bda101cdee34b066c45683cc11295b2dd
Buldakov Mikhail <mishabul2003@gmail.com>
2024-05-14 11:40:23 +0300

    More data sent

--- meet_match/net/backend/internal/models/card.go
+++ meet_match/net/backend/internal/models/card.go
@@ -1,9 +1,15 @@
 package models
 
 type Card struct {
-	Id          uint64
-	ImgUrl      string
-	CardName    string
-	Rating      int
-	Description string
+	Id             uint64
+	ImgUrl         string
+	CardName       string
+	Rating         *uint64 //  star fields can be nil
+	Description    *string
+	Subway         *string
+	Cost           *string
+	Timetable      *string
+	AgeRestriction *string
+	Phone          *string
+	SiteUrl        *string
 }
--- meet_match/net/backend/internal/models/models_dto/card.go
+++ meet_match/net/backend/internal/models/models_dto/card.go
@@ -3,19 +3,31 @@ package models_dto
 import "test_backend_frontend/internal/models"
 
 type Card struct {
-	Id          uint64 `json:"id"`
-	ImgUrl      string `json:"image"`
-	CardName    string `json:"title,card_name"`
-	Rating      int    `json:"rating,omitempty"`
-	Description string `json:"description,omitempty"`
+	Id             uint64  `json:"id"`
+	ImgUrl         string  `json:"image"`
+	CardName       string  `json:"title,card_name"`
+	Rating         *uint64 `json:"rating,omitempty"`
+	Description    *string `json:"description,omitempty"`
+	Subway         *string `json:"subway,omitempty"`
+	Cost           *string `json:"cost,omitempty"`
+	Timetable      *string `json:"timetable,omitempty"`
+	AgeRestriction *string `json:"age_restriction,omitempty"`
+	Phone          *string `json:"phone,omitempty"`
+	SiteUrl        *string `json:"site_url,omitempty"`
 }
 
 func ToDTOCard(card *models.Card) *Card {
 	return &Card{
-		Id:          card.Id,
-		ImgUrl:      card.ImgUrl,
-		CardName:    card.CardName,
-		Rating:      card.Rating,
-		Description: card.Description,
+		Id:             card.Id,
+		ImgUrl:         card.ImgUrl,
+		CardName:       card.CardName,
+		Rating:         card.Rating,
+		Description:    card.Description,
+		Subway:         card.Subway,
+		Cost:           card.Cost,
+		Timetable:      card.Timetable,
+		AgeRestriction: card.AgeRestriction,
+		Phone:          card.Phone,
+		SiteUrl:        card.SiteUrl,
 	}
 }
