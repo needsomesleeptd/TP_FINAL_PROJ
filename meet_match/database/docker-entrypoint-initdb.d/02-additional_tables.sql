50116bb8b5e999e533e65cbc3846bde1d193a346
Buldakov Mikhail <mishabul2003@gmail.com>
2024-05-15 10:28:38 +0300

    auto bd up

---
+++ meet_match/database/docker-entrypoint-initdb.d/02-additional_tables.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS feedbacks(
+                                        id SERIAL,
+                                        user_id INT NOT NULL,
+                                        description TEXT,
+                                        hasgone BOOL NOT NULL,
+                                        datetime timestamp NOT NULL,
+                                        PRIMARY KEY (id)
+
+);
+
+
+CREATE TABLE IF NOT EXISTS matches(
+                                      id SERIAL,
+                                      session_id uuid,
+                                      datetime timestamp NOT NULL,
+                                      got_feedback BOOL,
+                                      matched_card_id bigint,
+                                      PRIMARY KEY (id)
+);
